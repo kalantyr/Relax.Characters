691eea81def59a379c887f5867407933d31e20f9
kalavarda <kulerda.kalavarda@gmail.com>
2022-06-02 11:21:55 +0300

    1

---
+++ Relax.Characters/Controllers/CharacterController.cs
@@ -0,0 +1,18 @@
+﻿using Kalantyr.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Relax.Characters.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController: ControllerBase
+    {
+        [HttpGet]
+        [Route("allCharacters")]
+        public async Task<IActionResult> GetAllCharactersAsync(CancellationToken cancellationToken)
+        {
+            var userToken = Request.GetAuthToken();
+            throw new NotImplementedException();
+        }
+    }
+}
